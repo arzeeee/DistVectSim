4e8b0fdf289476c8c2e4a6fe23c4975fe4d2a0b6
arzeeee <rifkyanoa@gmail.com>
2017-11-20 16:33:32 +0700

    inisialisasi list dan dictionary untuk node

--- DV.py
+++ DV.py
@@ -1,5 +1,16 @@
-# import sys
-# x = sys.argv[1]
-# print(x)
+### ----Routing Table----	###
+### By Natus Vincere		###
+
 
 nNode = input("Masukkan jumlah node: ")
+NodeKe = {}                                 #Inisialisasi dictionary node
+for i in range(nNode):						#Inisiasi list tetangga kosong
+  NodeKe[i+1] = []
+
+for i in range(nNode):						#Inisialisasi node bertetangga
+  NodeT = input("Masukkan node yang bertetangga dengan node "+str(i+1)+" :")
+  for j in range(len(NodeT)):
+    NodeKe.setdefault(i+1,[]).append(NodeT[j])
+  
+print(NodeKe)
+
